f57a96abb7834a57f24b87016ce7aec270207bc6
James O'Neill <hemebond@gmail.com>
2019-12-31 18:17:11 +0900

    Update fixtures script to symlink screenshots. Fix card list view.

--- quaddicted/maps/fixtures/src/json_to_fixtures.py
+++ quaddicted/maps/fixtures/src/json_to_fixtures.py
@@ -18,11 +18,14 @@ all_packages = [
 	# map
 ]
 
+all_screenshots = []
+
 map_list = []
 
 map_pk = 0
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
+MEDIA_DIR = Path(BASE_DIR.joinpath('media'))
 
 for root, dirs, files in os.walk('.'):
 	for file in files:
@@ -60,21 +63,44 @@ for root, dirs, files in os.walk('.'):
 					}.get(tag_value, 'q4')
 					continue
 
-			package_file_path = os.path.join('maps',
-			                                 'by-sha256',
-			                                 j['sha256'][0],
-			                                 j['sha256'],
-			                                 j['filename'][0])
+			# Fixtures for map packages require
+			# actual files so here we create dummy files
+			# e.g., /home/james/Workspace/quaddicted/quaddicted/media/maps/by-sha256/6/6e42a183c0e1992069f53e235285eb3863d4d7fc33f0eb584f262223e174a381/dopa.zip
+			package_abs_path = MEDIA_DIR.joinpath('maps',
+			                                      'by-sha256',
+			                                      j['sha256'][0],
+			                                      j['sha256'],
+			                                      j['filename'][0])
+			# /home/james/Workspace/quaddicted/quaddicted/media/maps/by-sha256/6/6e42a183c0e1992069f53e235285eb3863d4d7fc33f0eb584f262223e174a381/dopa.jpg
+			screenshot_abs_path = package_abs_path.parent.joinpath(package_abs_path.stem + '.jpg')
+
 			all_packages.append({
-				'file': package_file_path,
+				'file': str(package_abs_path.relative_to(MEDIA_DIR)),
 				'file_name': j['filename'][0],
 				'file_hash': j['sha256'],
 				'created': fields['created'],
 				'map': map_pk,
 			})
+			package_abs_path.parent.mkdir(parents=True, exist_ok=True)
+			package_abs_path.touch()
 
-			Path(os.path.dirname(os.path.join(BASE_DIR, 'media', package_file_path))).mkdir(parents=True, exist_ok=True)
-			Path(os.path.join(BASE_DIR, 'media', package_file_path)).touch()
+			screenshot_src_path = BASE_DIR.joinpath(
+				'maps',
+				'fixtures',
+				'screenshots',
+				package_abs_path.stem + '.jpg'
+			)
+
+			# print('screenshot_src_path: %s' % screenshot_src_path)
+			# print('screenshot_abs_path: %s' % screenshot_abs_path)
+			if screenshot_src_path.exists():
+				if not screenshot_abs_path.exists():
+					screenshot_abs_path.symlink_to(screenshot_src_path)
+
+				all_screenshots.append({
+					'image': str(screenshot_abs_path.relative_to(MEDIA_DIR)),
+					'map': map_pk,
+				})
 
 			map_list.append({
 				'model': 'maps.map',
@@ -112,4 +138,11 @@ for pk, package in enumerate(all_packages):
 		'model': 'maps.mappackage',
 		'pk': pk,
 		'fields': package,
+	}]))
+
+for pk, screenshot in enumerate(all_screenshots):
+	print(yaml.dump([{
+		'model': 'maps.screenshot',
+		'pk': pk,
+		'fields': screenshot,
 	}]))
