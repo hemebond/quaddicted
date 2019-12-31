d0114fed37f5b78b20989d0d67e6c01209cd177a
James O'Neill <hemebond@gmail.com>
2019-12-31 18:26:46 +0900

    More card template fixes. Remove old fixtures script.

--- quaddicted/do_fixtures.sh
+++
@@ -1,8 +0,0 @@
-python ./manage.py loaddata maps/fixtures/maps.yaml
-python ./manage.py loaddata maps/fixtures/authors.yaml
-python ./manage.py loaddata maps/fixtures/tags.yaml
-cp media/maps/screenshots/drysorrow.jpg media/maps/screenshots/eeaa54f2e1c0156de552a87326c927d4bd962033891d920ca9fd61a21b9c7b1f.jpg
-cp media/maps/screenshots/1000cuts1a.jpg media/maps/screenshots/a05f5ba2d31ef96b1f1285ee9329108b8251886848394379a41717228370f6b5.jpg
-cp media/maps/screenshots/explorejam1.jpg media/maps/screenshots/44a1413452b1bcbcfd66ce78dea07007f4ddce6edd877063c966a274b60fad35.jpg
-cp media/maps/screenshots/runjumpexplore1.jpg media/maps/screenshots/cb89f1738583c0b2da5fbeee4168dccc4de7c352c91041224b359031a1033344.jpg
-cp media/maps/screenshots/koohoo.jpg media/maps/screenshots/1c30822266fbc22d8aabb1b1f914596da4345a84cc426101ee25b26446f698d1.jpg
