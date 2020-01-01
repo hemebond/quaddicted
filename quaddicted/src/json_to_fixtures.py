c5332ac6a5c9345872ec4707e3342c054871a3da
James O'Neill <hemebond@gmail.com>
2020-01-01 22:56:26 +0900

    Update json_to_fixtures for Packages. Move templates from Maps app to project.

--- quaddicted/src/json_to_fixtures.py
+++ quaddicted/src/json_to_fixtures.py
@@ -1,15 +1,28 @@
+"""
+└── src
+    ├── json
+    └── screenshots
+"""
+
 import os
+import sys
 import json
 import yaml
+import logging
 from pathlib import Path
 from django.utils.text import slugify
 
-all_authors = {
-	# name: [maps...]
-}
-all_tags = {
-	# name: [maps...]
-}
+
+make_files = True
+if len(sys.argv) > 1 and sys.argv[1] == '-noop':
+	make_files = False
+
+
+author_packages = {}
+author_names = {}
+
+tag_packages = {}
+tag_names = {}
 
 all_packages = [
 	# file
@@ -20,21 +33,20 @@ all_packages = [
 ]
 
 taggit_tag_names = {}
-all_taggits = {}
 
 all_screenshots = []
 
-map_list = []
+package_list = []
 
-map_pk = 0
+package_pk = 0
 
-BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_DIR = Path(BASE_DIR.joinpath('media'))
 
 for root, dirs, files in os.walk('.'):
 	for file in files:
 		if file.endswith('.json'):
-			map_pk += 1
+			package_pk += 1
 
 			with open(os.path.join(root, file)) as fp:
 				j = json.load(fp)
@@ -54,13 +66,23 @@ for root, dirs, files in os.walk('.'):
 					continue
 
 				if tag_name == 'author':
-					all_authors.setdefault(tag_value, []).append(map_pk)
+					author_username = slugify(tag_value)
+					author_fullname = tag_value
+
+					if author_username in author_names:
+						if author_fullname != author_names[author_username]:
+							logging.warning("Created duplicate author username slug: (%s: %s) (%s: %s)" % (author_username, author_fullname, author_username, author_names[author_username]))
+					else:
+						author_names[author_username] = author_fullname
+
+					author_packages.setdefault(author_username, []).append(package_pk)
 					continue
 
 				if tag_name == 'tag':
-					all_tags.setdefault(tag_value, []).append(map_pk)
 					# taggit requires unique slugs
-					all_taggits.setdefault(slugify(tag_value), []).append(map_pk)
+					tag_slug = slugify(tag_value)
+					tag_packages.setdefault(tag_slug, []).append(package_pk)
+					tag_names[tag_slug] = tag_value
 					continue
 
 				if tag_name == 'game':
@@ -71,28 +93,24 @@ for root, dirs, files in os.walk('.'):
 
 			# Fixtures for map packages require
 			# actual files so here we create dummy files
-			# e.g., /home/james/Workspace/quaddicted/quaddicted/media/maps/by-sha256/6/6e42a183c0e1992069f53e235285eb3863d4d7fc33f0eb584f262223e174a381/dopa.zip
-			package_abs_path = MEDIA_DIR.joinpath('maps',
-			                                      'by-sha256',
+			# e.g., /home/james/Workspace/quaddicted/quaddicted/media/packages/6/6e42a183c0e1992069f53e235285eb3863d4d7fc33f0eb584f262223e174a381/dopa.zip
+			package_abs_path = MEDIA_DIR.joinpath('packages',
 			                                      j['sha256'][0],
 			                                      j['sha256'],
 			                                      j['filename'][0])
-			# /home/james/Workspace/quaddicted/quaddicted/media/maps/by-sha256/6/6e42a183c0e1992069f53e235285eb3863d4d7fc33f0eb584f262223e174a381/dopa.jpg
+			# /home/james/Workspace/quaddicted/quaddicted/media/packages/6/6e42a183c0e1992069f53e235285eb3863d4d7fc33f0eb584f262223e174a381/dopa.jpg
 			screenshot_abs_path = package_abs_path.parent.joinpath(package_abs_path.stem + '.jpg')
 
-			all_packages.append({
-				'file': str(package_abs_path.relative_to(MEDIA_DIR)),
-				'file_name': j['filename'][0],
-				'file_hash': j['sha256'],
-				'created': fields['created'],
-				'map': map_pk,
-			})
-			package_abs_path.parent.mkdir(parents=True, exist_ok=True)
-			package_abs_path.touch()
+			fields['file'] = str(package_abs_path.relative_to(MEDIA_DIR))
+			fields['file_name'] = j['filename'][0]
+			fields['file_hash'] = j['sha256']
+
+			if make_files:
+				package_abs_path.parent.mkdir(parents=True, exist_ok=True)
+				package_abs_path.touch()
 
 			screenshot_src_path = BASE_DIR.joinpath(
-				'maps',
-				'fixtures',
+				'src',
 				'screenshots',
 				package_abs_path.stem + '.jpg'
 			)
@@ -100,78 +118,61 @@ for root, dirs, files in os.walk('.'):
 			# print('screenshot_src_path: %s' % screenshot_src_path)
 			# print('screenshot_abs_path: %s' % screenshot_abs_path)
 			if screenshot_src_path.exists():
-				if not screenshot_abs_path.exists():
+				if make_files and not screenshot_abs_path.exists():
 					screenshot_abs_path.symlink_to(screenshot_src_path)
 
 				all_screenshots.append({
 					'image': str(screenshot_abs_path.relative_to(MEDIA_DIR)),
-					'map': map_pk,
+					'package': package_pk,
 				})
 
-			map_list.append({
-				'model': 'maps.map',
-				'pk': map_pk,
+			package_list.append({
+				'model': 'packages.package',
+				'pk': package_pk,
 				'fields': fields
 			})
 
 
-print(yaml.dump(map_list))
+print(yaml.dump(package_list))
 
 
-for pk, (tag_name, tag_maps) in enumerate(all_tags.items()):
-	print(yaml.dump([{
-		'model': 'maps.tag',
-		'pk': pk,
-		'fields': {
-			'name': tag_name,
-			'maps': tag_maps
-		}
-	}]))
-
-
-for pk, (tag_name, tag_maps) in enumerate(all_taggits.items()):
+for pk, (tag_slug, tag_maps) in enumerate(tag_packages.items()):
 	print(yaml.dump([{
 		'model': 'taggit.tag',
 		'pk': pk,
 		'fields': {
-			'name': tag_name,
-			'slug': tag_name,
+			'name': tag_names[tag_slug],
+			'slug': tag_slug,
 		}
 	}]))
 
-	for tm in tag_maps:
+	for package_id in tag_maps:
 		print(yaml.dump([{
 			'model': 'taggit.taggeditem',
 			'pk': None,
 			'fields': {
 				'tag': pk,
-				'content_type': ['maps', 'map'],
-				'object_id': tm,
+				'content_type': ['packages', 'package'],
+				'object_id': package_id,
 			}
 		}]))
 
 
-for pk, (author_name, author_maps) in enumerate(all_authors.items()):
+for pk, (author_username, author_fullname) in enumerate(author_names.items()):
 	print(yaml.dump([{
-		'model': 'maps.author',
+		'model': 'packages.author',
 		'pk': pk,
 		'fields': {
-			'name': author_name,
-			'maps': author_maps
+			'username': author_username,
+			'fullname': author_fullname,
+			'packages': author_packages[author_username]
 		}
 	}]))
 
 
-for pk, package in enumerate(all_packages):
-	print(yaml.dump([{
-		'model': 'maps.mappackage',
-		'pk': pk,
-		'fields': package,
-	}]))
-
 for pk, screenshot in enumerate(all_screenshots):
 	print(yaml.dump([{
-		'model': 'maps.screenshot',
+		'model': 'packages.screenshot',
 		'pk': pk,
 		'fields': screenshot,
 	}]))
