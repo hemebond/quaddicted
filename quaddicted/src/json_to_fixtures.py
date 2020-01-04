f96679d2f2cfc7005ffee5000c1d76a87ee371a8
James O'Neill <hemebond@gmail.com>
2020-01-05 02:10:14 +0900

    Update fixtures script with author changes and allow reading of ratings.

--- quaddicted/src/json_to_fixtures.py
+++ quaddicted/src/json_to_fixtures.py
@@ -9,14 +9,18 @@ import sys
 import json
 import yaml
 import logging
+from operator import itemgetter
 from pathlib import Path
 from django.utils.text import slugify
+from datetime import datetime
+
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 make_files = True
 if len(sys.argv) > 1 and sys.argv[1] == '-noop':
 	make_files = False
-
 
 author_packages = {}
 author_names = {}
@@ -36,6 +40,33 @@ SRC_DIR = Path(__file__).resolve().parent
 BASE_DIR = SRC_DIR.parent
 MEDIA_DIR = Path(BASE_DIR.joinpath('media'))
 
+
+
+#
+# Read in ratings
+#
+ratings = []
+ratings_raw = {}
+with open('ratings.json', 'r') as fp:
+	ratings_json = json.load(fp)
+
+logging.debug("Read %s ratings" % len(ratings_json))
+
+for rating in ratings_json:
+	ratings_raw.setdefault(rating['zipname'], []).append({
+		'datetime': (datetime.fromtimestamp(rating['timestamp'])).isoformat() + 'Z',
+		'rating': rating['rating_value'],
+		'username': rating['username'],
+	})
+
+logging.debug("Found ratings for %s packages" % len(ratings_raw.keys()))
+
+
+
+#
+# Process the packages/maps
+#
+rating_pk = 0
 for root, dirs, files in os.walk(SRC_DIR.joinpath('json')):
 	for file in files:
 		if file.endswith('.json'):
@@ -44,11 +75,13 @@ for root, dirs, files in os.walk(SRC_DIR.joinpath('json')):
 			with open(os.path.join(root, file)) as fp:
 				j = json.load(fp)
 
-			fields = {
-			}
+			fields = {}
 
 			for tag in j['tags']:
 				tag_name, tag_value = tag.split('=', 1)
+
+				tag_name = tag_name.strip()
+				tag_value = tag_value.strip()
 
 				if tag_name == 'title':
 					fields['name'] = tag_value
@@ -59,16 +92,16 @@ for root, dirs, files in os.walk(SRC_DIR.joinpath('json')):
 					continue
 
 				if tag_name == 'author':
-					author_username = slugify(tag_value)
-					author_fullname = tag_value
+					author_slug = slugify(tag_value)
+					author_name = tag_value
 
-					if author_username in author_names:
-						if author_fullname != author_names[author_username]:
-							logging.warning("Created duplicate author username slug: (%s: %s) (%s: %s)" % (author_username, author_fullname, author_username, author_names[author_username]))
+					if author_slug in author_names:
+						if author_name != author_names[author_slug]:
+							logging.warning("Created duplicate author slug: (%s: %s) (%s: %s)" % (author_slug, author_name, author_slug, author_names[author_slug]))
 					else:
-						author_names[author_username] = author_fullname
+						author_names[author_slug] = author_name
 
-					author_packages.setdefault(author_username, []).append(package_pk)
+					author_packages.setdefault(author_slug, []).append(package_pk)
 					continue
 
 				if tag_name == 'tag':
@@ -125,10 +158,33 @@ for root, dirs, files in os.walk(SRC_DIR.joinpath('json')):
 				'fields': fields
 			})
 
+			# Create ratings
+			try:
+				package_ratings = ratings_raw[package_abs_path.stem]
+			except KeyError:
+				logging.warning('No package for rating %s' % package_abs_path.stem)
+			else:
+				for rating in package_ratings:
+					rating_pk += 1
+					ratings.append({
+						'model': 'quaddicted_packages.rating',
+						'pk': rating_pk,
+						'fields': {
+							'user': rating['username'],
+							'package': package_pk,
+							'rating': rating['rating'],
+							'created': rating['datetime'],
+						}
+					})
+
+				del(ratings_raw[package_abs_path.stem])
+
+logging.debug(ratings_raw)
 
 print(yaml.dump(package_list))
+print(yaml.dump(ratings))
 
-
+package_tag_pk = 0
 for pk, (tag_slug, tag_maps) in enumerate(tag_packages.items()):
 	print(yaml.dump([{
 		'model': 'taggit.tag',
@@ -140,9 +196,10 @@ for pk, (tag_slug, tag_maps) in enumerate(tag_packages.items()):
 	}]))
 
 	for package_id in tag_maps:
+		package_tag_pk += 1
 		print(yaml.dump([{
 			'model': 'taggit.taggeditem',
-			'pk': None,
+			'pk': package_tag_pk,
 			'fields': {
 				'tag': pk,
 				'content_type': ['quaddicted_packages', 'package'],
@@ -151,14 +208,14 @@ for pk, (tag_slug, tag_maps) in enumerate(tag_packages.items()):
 		}]))
 
 
-for pk, (author_username, author_fullname) in enumerate(author_names.items()):
+for pk, (author_slug, author_name) in enumerate(author_names.items()):
 	print(yaml.dump([{
 		'model': 'quaddicted_packages.author',
 		'pk': pk,
 		'fields': {
-			'username': author_username,
-			'fullname': author_fullname,
-			'packages': author_packages[author_username]
+			'slug': author_slug,
+			'name': author_name,
+			'packages': author_packages[author_slug]
 		}
 	}]))
 
