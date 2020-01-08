ea3bedf2b9773cb8d587c68644d1bd3590766423
James O'Neill <hemebond@gmail.com>
2020-01-08 17:56:15 +0900

    Move project-wide static files.

--- quaddicted/quaddicted/settings/development.py
+++ quaddicted/quaddicted/settings/development.py
@@ -17,8 +17,9 @@ DATABASES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'quaddicted', 'static'),
 ]
 
 # Save all uploads to the project directory during development
