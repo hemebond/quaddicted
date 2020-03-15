d94a1ce737d17869ee1a40def7905b653429c1bc
James O'Neill <hemebond@gmail.com>
2020-03-15 21:46:36 +0900

    Register quaddicted comment flag model with admin.

--- quaddicted/comments/admin.py
+++ quaddicted/comments/admin.py
@@ -1,8 +1,13 @@
-# from django.contrib import admin
+from django.contrib import admin
 # from django_comments.admin import CommentsAdmin
 # from django_comments.models import Comment as DCComment
+from django_comments.models import CommentFlag
 # from .models import QuaddictedCommentModel
 
 
 # admin.site.register(QuaddictedCommentModel, CommentsAdmin)
 # admin.site.register(DCComment, CommentsAdmin)
+
+@admin.register(CommentFlag)
+class CommentFlagAdmin(admin.ModelAdmin):
+	list_display = ('user', 'flag', 'flag_date')
